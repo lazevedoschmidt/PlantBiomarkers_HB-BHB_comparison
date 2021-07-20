c2c3230dde894926bebdff58a79bd98858c424a3
Lauren Azevedo Schmidt <71849192+lazevedoschmidt@users.noreply.github.com>
2021-07-20 09:49:01 -0600

    Delete geom_signif code.R

--- geom_signif code.R
+++
@@ -1,7 +0,0 @@
-#Geom_signif code
-geom_signif(comparisons = list(c("Paleocene","PETM")),
-  map_signif_level=TRUE)+
-geom_signif(comparisons = list(c("PETM","Eocene")),
-  map_signif_level=TRUE)+
-geom_signif(comparisons = list(c("Paleocene","Eocene")),
-  map_signif_level=TRUE)
